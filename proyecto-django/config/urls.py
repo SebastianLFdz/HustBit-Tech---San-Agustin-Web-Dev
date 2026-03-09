c68ae7728b339ac4f75aa1ca2b2f28f052f6fe0e
Talkative <126505369+SebastianLFdz@users.noreply.github.com>
2026-03-09 20:47:21 +0000

    Modificacion 9

--- proyecto-django/config/urls.py
+++ proyecto-django/config/urls.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.view),
+    path('admin/', admin.site.urls),
     path('', include('core.urls')), # Esto conecta tu app core
 ]
