2a67d918d0d53da06976929c3592a48aeeb93cc5
Talkative <126505369+SebastianLFdz@users.noreply.github.com>
2026-04-07 01:29:40 +0000

    Restauración manual de index.html y referencias.html

--- proyecto-django/core/forms.py
+++
@@ -1,6 +0,0 @@
-from django import forms
-
-class ContactoForm(forms.Form):
-    nombre = forms.CharField(max_length=100, label="Tu Nombre")
-    correo = forms.EmailField(label="Tu Correo Electrónico")
-    mensaje = forms.CharField(widget=forms.Textarea, label="¿En qué podemos ayudarte?")
